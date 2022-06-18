13854a56a332c7b1efbc8054f403c08776661b5b
hanglong4000 <sapbatcheung@gmail.com>
2022-06-18 13:52:33 +0700

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
-import type { NextPage } from 'next'
-
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
-      Welcome to the new era
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
+  );
+};
 
-        
-
-        
-  )
-}
-
-export default Home
+export default Home;
